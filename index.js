fa78022459c8b6a83036bd4207ca90741262bab1
sujilnt <sujilnt@gmail.com>
2018-10-10 14:42:06 +0100

    updating url

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const fetch =require("node-fetch");
-const token="8f0015f3e2df912582c8";
-const AppId="9760a8b42968dc9790e1";
-const permenanent_token= "cff216bbcfc55e1d5820";
-const AppSecret="ed3d5f61298bc31f31f7";
-const url = `https://api.dexcell.com/v3/oauth/access-token?temp_token=${token}&secret=${AppSecret}&app_id=${AppId}` ;
 
   const fetchdata={
-   credentials: 'include',
-   mode: 'cors',
-  method: 'POST', // or 'PUT',
   headers:{
+    'Content-Type': 'application/json;charset=UTF-8',
     'x-dexcell-token': 'cff216bbcfc55e1d5820',
   }
     };
-fetch("https://optimisedbuildings.dexcell.com/devices?start=50&limit=200",fetchdata).then(function (data){
+fetch("http://api.dexcell.com/v3/locations/#4/tags",fetchdata).then(function (data){
   console.log("data",data);
 });
-
-console.log(fetch);
